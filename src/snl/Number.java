5ae9dff60723d66c5dd41ee01a3a9564d7c9f20e
Nico Villagomez <95368240+LucidPessimist@users.noreply.github.com>
2021-12-08 09:00:53 -0800

    IM GETTIN THERE

--- src/snl/Number.java
+++ src/snl/Number.java
@@ -5,20 +5,13 @@ public class Number
 {
     private int row;
     private int column;
-    private int num;
-    
- Number(int _row,int _column,int _num)
+    private int _num;
+
+    Number(int xdelta, int ydelta, int num) 
     {
-        row = _row;
-        column = _column;
-        num = _num;
-    }
-
-
-    public void Number(int _row,int _column,int _num) {
-        row = _row;
-        column = _column;
-        num = _num;     
+        row = xdelta;
+        column = ydelta;
+        _num = num;
     }
     
 public void draw(Graphics2D g,int xdelta,int ydelta) 
@@ -26,6 +19,7 @@ public void draw(Graphics2D g,int xdelta,int ydelta)
     g.setColor(Color.green);
     int zcolumn = column;
     int zrow = row;
+    int num = _num;
     for (int i=0;i<num;i++) 
     {
         g.drawString("" + num, Window.getX(ydelta), Window.getX(xdelta*9-1));
@@ -36,8 +30,11 @@ public void draw(Graphics2D g,int xdelta,int ydelta)
         ydelta*=num;
         }
         else if (num <= 100)
+        {
         zcolumn++;
         xdelta*=zcolumn-6;
+        }
+
     }
 }
 }
